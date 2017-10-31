f822dd45e5d2d982ad7d8b21a9684a2d80bdeed8
justinhyou <hyobinyou@gmail.com>
2017-10-30 23:11:38 -0700

    update confusion matrix

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,9 +1,21 @@
-import matplotlib.pyplot as plt
 import numpy as np
-
-m = np.array([[1, 0, 0, 0], [0, 1, 1, 1], [0, 1, 0, 1], [1, 0, 0, 1]])
-
-
-plt.matshow(m)
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
+classes = np.array([1,2])
+cm = np.array(([1,2],[0,1]))
+plt.figure()
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
 plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+fmt = '.2f' if False else 'd'
+thresh = cm.max() / 2.
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
 plt.show()
