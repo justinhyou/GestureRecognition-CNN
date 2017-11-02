f2a81d0dd97bda734abf92a95f27698e5fd8d1ef
justinhyou <hyobinyou@gmail.com>
2017-11-02 12:28:53 -0700

    update classifier and output

---
+++ classify_JIGSAW.py
@@ -0,0 +1,92 @@
+import sys
+import os
+import csv
+import glob
+import shutil
+import numpy as np
+import tensorflow as tf
+from keras.utils.np_utils import probas_to_classes
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as k
+from keras.models import model_from_json
+
+# fix seed for reproducible results (only works on CPU, not GPU)
+seed = 9
+np.random.seed(seed=seed)
+tf.set_random_seed(seed=seed)
+
+# parameters dependent on your dataset: modified to your example
+img_width, img_height = 100, 100  # must match the fix size of your train image sizes. 600, 150 for text_images
+batch_size = 32  # try 4, 8, 16, 32, 64, 128, 256 dependent on CPU/GPU memory capacity (powers of 2 values).
+
+# default paths
+model_name = 'model.json'
+model_weights = 'top_model_weights.h5'
+results_name = 'predictions.csv'
+
+
+def classify(trained_model_dir, test_data_dir, results_path):
+    # load json and create model
+    json_file = open(os.path.join(trained_model_dir, model_name), 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    model.load_weights(os.path.join(trained_model_dir, model_weights))
+
+    # Read Data
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_generator = test_datagen.flow_from_directory(test_data_dir,
+                                                      target_size=(img_width, img_height),
+                                                      batch_size=batch_size,
+                                                      shuffle=False)
+
+    # Calculate class posteriors probabilities
+    y_probabilities = model.predict_generator(test_generator,
+                                              val_samples=test_generator.nb_sample)
+    # Calculate class labels
+    y_classes = probas_to_classes(y_probabilities)
+    filenames = [filename.split('/')[1] for filename in test_generator.filenames]
+    ids = [filename.split('.')[0] for filename in filenames]
+
+    # save results as a csv file in the specified results directory
+    with open(os.path.join(results_path, results_name), 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(('id', 'class1_prob', 'class2_prob', 'class3_prob', 'class4_prob', \
+                        'class5_prob', 'class6_prob', 'class8_prob', 'class9_prob', \
+                        'class10_prob', 'class11_prob', 'label')) 
+        #writer.writerows(zip(ids, y_probabilities[:, 0], y_probabilities[:, 1], y_classes))
+        for i in range(len(y_classes)):
+            writer.writerow((ids[i], y_probabilities[i][0], y_probabilities[i][1], y_probabilities[i][2], \
+                            y_probabilities[i][3], y_probabilities[i][4], y_probabilities[i][5], \
+                            y_probabilities[i][6], y_probabilities[i][7], y_probabilities[i][8], \
+                            y_probabilities[i][9], y_classes[i] ))
+
+    # # semi-supervise learning: save the classified test data in their respective train class folder to increase
+    # # your training dataset to include training
+    # validation_data_dir = os.path.join(test_data_dir, '../validation')
+    # validation_datagen = ImageDataGenerator(rescale=1. / 255)
+    # validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
+    #                                                               target_size=(img_width, img_height),
+    #                                                               batch_size=batch_size,
+    #                                                               shuffle=False)
+    # labels_dictionary = dict((value, key) for key, value in validation_generator.class_indices.items())
+    # for i, filename in enumerate(glob.glob(os.path.join(test_data_dir, 'test', '*.*'))):
+    #     if y_probabilities[i, y_classes[i]] > .9995:
+    #         shutil.copy(filename, os.path.join(test_data_dir, '../train', labels_dictionary.get(y_classes[i])))
+
+
+if __name__ == '__main__':
+    if not len(sys.argv) == 4:
+        print('Arguments must match:\npython code/classify.py <model_dir/> <test_dir/> <results_dir/>')
+        print('Example: python code/classify.py model/dogs_cats data/dogs_cats/test/ results/dogs_cats/')
+        sys.exit(2)
+    else:
+        model_dir = os.path.abspath(sys.argv[1])
+        test_dir = os.path.abspath(sys.argv[2])
+        results_dir = os.path.abspath(sys.argv[3])
+        os.makedirs(results_dir, exist_ok=True)
+
+    classify(model_dir, test_dir, results_dir) 
+
+    # release memory
+    k.clear_session()
